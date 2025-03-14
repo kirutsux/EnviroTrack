d2a1295e12526aa12cd276fc3f7599a6656ed930
Loys <wagasloys@gmail.com>
2025-03-15 01:55:45 +0800

    sp,registraions

--- app/src/main/java/com/ecocp/capstoneenvirotrack/businesses/COMP_PCOAccreditation.kt
+++ app/src/main/java/com/ecocp/capstoneenvirotrack/businesses/COMP_PCOAccreditation.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.ecocp.capstoneenvirotrack.R
 
-class COMP_PCORegistration : AppCompatActivity() {
+class COMP_PCOAccreditation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_comp_registration)
+        setContentView(R.layout.activity_comp_accreditation)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
