1f1ec1aaa5da2dc3085291f8eb2c801085308785
Loys <wagasloys@gmail.com>
2025-09-14 01:41:34 +0800

    UI Goods

---
+++ app/src/main/java/com/ecocp/capstoneenvirotrack/view/businesses/COMP_Module3.kt
@@ -0,0 +1,20 @@
+package com.ecocp.capstoneenvirotrack.view.businesses
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ecocp.capstoneenvirotrack.R
+
+class COMP_Module3 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_comp_module3, container, false)
+    }
+
+}
