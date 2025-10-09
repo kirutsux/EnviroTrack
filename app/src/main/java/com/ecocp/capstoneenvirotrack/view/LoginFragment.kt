6b15cca1a7b777d45f9f79a2edc1c3fad135f572
mived1st <joshymive@gmail.com>
2025-10-09 21:00:31 +0800

    Merging updates

--- app/src/main/java/com/ecocp/capstoneenvirotrack/view/LoginFragment.kt
+++ app/src/main/java/com/ecocp/capstoneenvirotrack/view/LoginFragment.kt
@@ -1,14 +1,18 @@
+@file:Suppress("DEPRECATION")
+
 package com.ecocp.capstoneenvirotrack.view
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
